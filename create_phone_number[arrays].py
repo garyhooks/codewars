4697ea436ca86492c3414f35328811e3df21860e
Gary Hooks <garyhooks@gmail.com>
2018-06-27 14:00:04 +0100

    Create create_phone_number[arrays].py

---
+++ create_phone_number[arrays].py
@@ -0,0 +1,12 @@
+def create_phone_number(n):
+    
+    phone_number = ""
+    
+    phone_number = "(" + str(n[0]) + str(n[1]) + str(n[2]) + ") "
+    phone_number += str(n[3]) + str(n[4]) + str(n[5]) + "-"
+    phone_number += str(n[6]) + str(n[7]) + str(n[8]) + str(n[9])
+    
+    print(n)
+        
+        
+    return phone_number
